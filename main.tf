5cb715d4e7cea0f35375d6524bc8cf391eafd461
Obioma Osuagwu <justobioma@outlook.com>
2024-09-04 14:15:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "blog" {
 resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"  
   from_port   = 80
-  tcp_port    = 80
+  to_port    = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
@@ -47,7 +47,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 resource "aws_security_group_rule" "blog_https_in" {
   type = "ingress"  
   from_port = 443
-  tcp_port  = 443
+  to_port  = 443
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
@@ -56,7 +56,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 resource "aws_security_group_rule" "blog_everything_out" {
   type = "egress"  
   from_port = 0
-  tcp_port  = 0
+  to_port  = 0
   protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
